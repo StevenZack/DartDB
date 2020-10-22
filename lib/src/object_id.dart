7ec7cce7d5a7204bd635a936ba603a7cdba97d24
stevenzack <zigzigcheers@gmail.com>
2020-10-22 10:12:57 +0800

    objectid

--- lib/src/object_id.dart
+++ lib/src/object_id.dart
@@ -2,13 +2,18 @@ library dart_db;
 
 import 'dart:typed_data';
 import 'dart:io';
+import 'dart:math';
 
 class ObjectID {
+  static final _random = Random();
+  static const _trippleUint8 = 256 * 256 * 256;
+
   Uint8List _data = Uint8List(12);
-  static int _counter = 0;
+
   ObjectID(Uint8List data) {
     _data = data;
   }
+
   factory ObjectID.now() {
     return ObjectID._fromTimestamp(DateTime.now());
   }
@@ -19,21 +24,30 @@ class ObjectID {
     final s = dateTime.millisecondsSinceEpoch ~/ 1000;
     final bytes = Uint8List(12);
     bytes[0] = s >> 8 * 3;
-    bytes[1] = s >> 8 * 2 % 256;
-    bytes[2] = s >> 8 % 256;
+    bytes[1] = (s >> 8 * 2) % 256;
+    bytes[2] = (s >> 8) % 256;
     bytes[3] = s % 256;
 
-    // machine id 3
+    // random id 3
+    final randomId = _random.nextInt(_trippleUint8);
+    bytes[4] = (randomId >> 8 * 2) % 256;
+    bytes[5] = (randomId >> 8) % 256;
+    bytes[6] = randomId % 256;
 
     // pid 3
     bytes[7] = pid >> 8 & 256;
     bytes[8] = pid % 256;
-    // rand 3
-    _counter++;
-    bytes[9] = _counter >> 8 * 2 % 256;
-    bytes[10] = _counter >> 8 % 256;
-    bytes[11] = _counter % 256;
     
+    // rand 3
+    final counter = _random.nextInt(_trippleUint8);
+    bytes[9] = (counter >> 8 * 2) % 256;
+    bytes[10] = (counter >> 8) % 256;
+    bytes[11] = counter % 256;
+
     return ObjectID(bytes);
+  }
+
+  Uint8List bytes() {
+    return _data;
   }
 }
--- test/my_test.dart
+++ test/my_test.dart
@@ -1,8 +1,8 @@
 import 'dart:typed_data';
 import 'dart:io';
 
+import 'package:dart_db/src/object_id.dart';
+
 main(List<String> args) {
-  print(pid);
-  print(pid);
-  print(pid);
+  print(ObjectID.now().bytes());
 }
